034c945cbb4d0841c5bf0f521a50a74e038450e5
KeisukeFuruta <kcomethunter@gmail.com>
2024-11-03 17:00:18 +0900

    初回push

---
+++ src/main/java/raisetech/StudentManagement/Application.java
@@ -0,0 +1,23 @@
+package raisetech.StudentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		// localhost:8080
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello,World";
+	}
+
+
+}
