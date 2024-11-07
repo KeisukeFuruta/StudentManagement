07e99ac050d0e42d3b69893f57571616d2796e63
KeisukeFuruta <kcomethunter@gmail.com>
2024-11-07 18:16:04 +0900

    第24回課題の削除

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -18,29 +18,11 @@ public class StudentService {
   }
 
   public List<Student> searchStudentList() {
-    // 検索処理
-    List<Student> students = repository.search();
-
-    // 絞り込みをする。年齢が30代以上の人のみ抽出する。
-    // 抽出したリストをコントローラーに返す。
-    List<Student> filteredStudents = students.stream()
-        .filter(student -> student.getAge() >= 30)
-        .toList();
-
-    return filteredStudents;
+    return repository.search();
   }
 
   public List<StudentCourse> searchStudentsCourseList() {
-    // 絞り込み検索で「Javaコース」のコース情報のみを抽出する。
-    // 抽出したリストをコントローラーに返す。
-
-    List<StudentCourse> studentCourseList = repository.searchStudentsCourses();
-
-    List<StudentCourse> javaOnly = studentCourseList.stream()
-        .filter(studentCourse -> studentCourse.getCourseName().equals("Javaコース"))
-        .toList();
-
-    return javaOnly;
+    return repository.searchStudentsCourses();
   }
 
 }
