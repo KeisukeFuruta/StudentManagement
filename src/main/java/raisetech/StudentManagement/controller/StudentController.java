a3713f3fe47f645fdf574ab440448094f197543c
KeisukeFuruta <kcomethunter@gmail.com>
2024-12-29 16:29:44 +0900

    単一検索の復元

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,6 +65,13 @@ public class StudentController {
     service.registerStudentCourse(studentDetail);
 
     return "redirect:/studentList";
+  }
+
+  @GetMapping("/student/{studentId}")
+  public String getStudent(@PathVariable String studentId, Model model) {
+    StudentDetail studentDetail = service.searchStudentDetail(studentId);
+    model.addAttribute("studentDetail", studentDetail);
+    return "updateStudent";
   }
 
   @PostMapping("/updateStudent")
