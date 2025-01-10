973f6291785814c85f9ae2590f7029136301cc28
KeisukeFuruta <kcomethunter@gmail.com>
2025-01-10 19:01:54 +0900

    XMLファイルの追加 受講生全件検索と単一検索のXML化

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -20,7 +20,6 @@ public interface StudentRepository {
    *
    * @return 受講生一覧（全件）
    */
-  @Select("SELECT * FROM students WHERE is_deleted = false")
   List<Student> search();
 
   /**
@@ -29,7 +28,6 @@ public interface StudentRepository {
    * @param studentId 受講生ID
    * @return 受講生
    */
-  @Select("SELECT * FROM students WHERE student_id = #{id}")
   Student searchStudent(String studentId);
 
   /**
